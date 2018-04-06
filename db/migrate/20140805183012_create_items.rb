1d424f6c639c1d28921f683b38f71467564845b6
Richard Steinbach <steinbach.dick@gmail.com>
2018-04-06 13:35:52 -0700

    flatiron-store-project-v-000

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :title
+      t.float :price
+      t.integer :inventory
+    end
 
   end
 end
